fe6a5baa9e30518f6bffd7c49f4ead8673493df4
Patryk Dudzinski (LemonX) <dudzinski.patryk.02@gmail.com>
2024-10-21 01:00:06 +0200

    fix

--- init-build.sh
+++ init-build.sh
@@ -7,11 +7,11 @@ python3 -m venv .venv
 source .venv/bin/activate
 pip install -r scripts/python/requirements.txt
 
-if [ "$(id -u)" -e 0 ]; then
+if [[ "$(id -u)" -eq 0 ]]; then
   sudo apt-get install -y cmake ninja-build clang ccache
 fi
 
-submodules/ariadna_constants/./generate-files.sh
+src/ariadna_constants/./generate-files.sh
 
 cmake -B build -G "Ninja" -DCOPY_COMPILE_COMMANDS=ON
 cmake --build build
--- src/version.hpp
+++ src/version.hpp
@@ -2,7 +2,7 @@
 #define VERSION_HPP
 
 #define VERSION_MAJOR 3
-#define VERSION_MINOR 186
-#define VERSION_BUILD 0
+#define VERSION_MINOR 188
+#define VERSION_BUILD 1
 
 #endif // VERSION_HPP
