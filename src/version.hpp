2e2cb24671210b47331b9f8fdf93cb016e16b5ee
patryk LapUbuntu <dodpat02@gmail.com>
2024-12-05 22:41:42 +0100

    update

--- src/version.hpp
+++ src/version.hpp
@@ -2,11 +2,11 @@
 #define VERSION_HPP
 
 #define VERSION_MAJOR 3
-#define VERSION_MINOR 205
-#define VERSION_BUILD 4
+#define VERSION_MINOR 206
+#define VERSION_BUILD 0
 
 // for easy find  in a .bin file
-#define VERSION_DEF_BUILD_STRING_DEF "build_version:3.205.4"
+#define VERSION_DEF_BUILD_STRING_DEF "build_version:3.206.0"
 static const char* VERSION_DEF_BUILD_STRING = VERSION_DEF_BUILD_STRING_DEF;
 
 
