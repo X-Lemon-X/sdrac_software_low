8adf410bb950541e19c2004f8cdf8142a0f1f983
patryk LapUbuntu <dodpat02@gmail.com>
2024-12-12 16:22:27 +0100

    Update submodules

--- src/version.hpp
+++ src/version.hpp
@@ -2,11 +2,11 @@
 #define VERSION_HPP
 
 #define VERSION_MAJOR 3
-#define VERSION_MINOR 209
-#define VERSION_BUILD 23
+#define VERSION_MINOR 210
+#define VERSION_BUILD 5
 
 // for easy find  in a .bin file
-#define VERSION_DEF_BUILD_STRING_DEF "build_version:3.209.23"
+#define VERSION_DEF_BUILD_STRING_DEF "build_version:3.210.5"
 static const char* VERSION_DEF_BUILD_STRING = VERSION_DEF_BUILD_STRING_DEF;
 
 
