811592a972dc5f8fba26950b21c859493b9ea9a7
patryk LapUbuntu <dodpat02@gmail.com>
2024-12-12 17:44:27 +0100

    update subdmoules

--- src/version.hpp
+++ src/version.hpp
@@ -2,11 +2,11 @@
 #define VERSION_HPP
 
 #define VERSION_MAJOR 3
-#define VERSION_MINOR 210
-#define VERSION_BUILD 5
+#define VERSION_MINOR 211
+#define VERSION_BUILD 0
 
 // for easy find  in a .bin file
-#define VERSION_DEF_BUILD_STRING_DEF "build_version:3.210.5"
+#define VERSION_DEF_BUILD_STRING_DEF "build_version:3.211.0"
 static const char* VERSION_DEF_BUILD_STRING = VERSION_DEF_BUILD_STRING_DEF;
 
 
