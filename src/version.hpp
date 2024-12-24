856dd12ba3638f585a42091f91631c9ac0e44e08
patryk LapUbuntu <dodpat02@gmail.com>
2024-12-24 15:19:43 +0100

    update submodueles

--- src/version.hpp
+++ src/version.hpp
@@ -2,11 +2,11 @@
 #define VERSION_HPP
 
 #define VERSION_MAJOR 4
-#define VERSION_MINOR 222
-#define VERSION_BUILD 0
+#define VERSION_MINOR 223
+#define VERSION_BUILD 18
 
 // for easy find  in a .bin file
-#define VERSION_DEF_BUILD_STRING_DEF "build_version:4.222.0"
+#define VERSION_DEF_BUILD_STRING_DEF "build_version:4.223.18"
 static const char* VERSION_DEF_BUILD_STRING = VERSION_DEF_BUILD_STRING_DEF;
 
 
