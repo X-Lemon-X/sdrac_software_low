bcc8214d9095f148122a3cf83888cafbb8fd26a7
Patryk Dudzinski (LemonX) <dudzinski.patryk.02@gmail.com>
2025-02-02 20:03:02 +0100

    update usbmodule

--- src/version.hpp
+++ src/version.hpp
@@ -2,11 +2,11 @@
 #define VERSION_HPP
 
 #define VERSION_MAJOR 4
-#define VERSION_MINOR 241
-#define VERSION_BUILD 6
+#define VERSION_MINOR 244
+#define VERSION_BUILD 0
 
 // for easy find  in a .bin file
-#define VERSION_DEF_BUILD_STRING_DEF "build_version:4.241.6"
+#define VERSION_DEF_BUILD_STRING_DEF "build_version:4.244.0"
 static const char* VERSION_DEF_BUILD_STRING = VERSION_DEF_BUILD_STRING_DEF;
 
 
