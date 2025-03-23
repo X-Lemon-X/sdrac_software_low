f6ddd3cf8693f2ded3e57e376c95e990b5ba4362
Patryk Dudzinski (LemonX) <dudzinski.patryk.02@gmail.com>
2025-03-23 23:53:52 +0100

    update submodules

--- src/version.hpp
+++ src/version.hpp
@@ -2,11 +2,11 @@
 #define VERSION_HPP
 
 #define VERSION_MAJOR 4
-#define VERSION_MINOR 260
-#define VERSION_BUILD 0
+#define VERSION_MINOR 261
+#define VERSION_BUILD 9
 
 // for easy find  in a .bin file
-#define VERSION_DEF_BUILD_STRING_DEF "build_version:4.260.0"
+#define VERSION_DEF_BUILD_STRING_DEF "build_version:4.261.9"
 static const char* VERSION_DEF_BUILD_STRING = VERSION_DEF_BUILD_STRING_DEF;
 
 
