ab68f81512033a6961da4e996eba069bbcc666c3
Patryk Dudzinski (LemonX) <dudzinski.patryk.02@gmail.com>
2025-04-15 16:35:53 +0200

    version update

--- src/version.hpp
+++ src/version.hpp
@@ -2,11 +2,11 @@
 #define VERSION_HPP
 
 #define VERSION_MAJOR 4
-#define VERSION_MINOR 266
-#define VERSION_BUILD 0
+#define VERSION_MINOR 267
+#define VERSION_BUILD 2
 
 // for easy find  in a .bin file
-#define VERSION_DEF_BUILD_STRING_DEF "build_version:4.266.0"
+#define VERSION_DEF_BUILD_STRING_DEF "build_version:4.267.2"
 static const char* VERSION_DEF_BUILD_STRING = VERSION_DEF_BUILD_STRING_DEF;
 
 
