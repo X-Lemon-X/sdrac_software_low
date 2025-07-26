dce6b0044c46acf1f94ee44ef701443db4c9c7d4
Patryk Dudziński <dodpat02@gmail.com>
2025-07-26 19:40:39 +0200

    update config

--- src/version.hpp
+++ src/version.hpp
@@ -2,11 +2,11 @@
 #define VERSION_HPP
 
 #define VERSION_MAJOR 4
-#define VERSION_MINOR 270
-#define VERSION_BUILD 0
+#define VERSION_MINOR 272
+#define VERSION_BUILD 2
 
 // for easy find  in a .bin file
-#define VERSION_DEF_BUILD_STRING_DEF "build_version:4.270.0"
+#define VERSION_DEF_BUILD_STRING_DEF "build_version:4.272.2"
 static const char* VERSION_DEF_BUILD_STRING = VERSION_DEF_BUILD_STRING_DEF;
 
 
